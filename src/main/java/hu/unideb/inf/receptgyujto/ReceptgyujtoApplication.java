2cb88e06a829df404e13eaffc9a72add96b59d32
Monori Imre <monorimi14@mailbox.unideb.hu>
2025-10-27 17:17:00 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/receptgyujto/ReceptgyujtoApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.receptgyujto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptgyujtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptgyujtoApplication.class, args);
+	}
+
+}
